b1f1871f6b99060f10004eb672022f4131517e0f
dmi3y <lapshukov@gmail.com>
2023-01-08 19:55:55 -0800

    feat: use image and webp formats

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+      logLevel: "debug",
+    }),
+  ],
 });
