24f6a31c186f6226517a4278638d134b5421cec0
dmi3y <dmitrii@matternet.us>
2023-09-27 22:41:40 -0700

    feat: moving to content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+const site =
+  process.env.SITE_URL || `https://${process.env.PUBLIC_VERCEL_BRANCH_URL}`;
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site,
+  output: "static",
+  integrations: [mdx(), sitemap()],
 });
