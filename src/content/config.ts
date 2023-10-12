04c134cdd7634ae563072194b499daf10245d217
dmi3y <dmitrii@matternet.us>
2023-10-11 21:07:34 -0700

    feat: move wallpaper source to content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,11 @@ import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
 const pages = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: () =>
+  schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
+      wallpapers: z.array(image()),
     }),
 });
 // 3. Export a single `collections` object to register your collection(s)
