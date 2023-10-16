e0fc96e6cf818dad3f123031f3008f87f038903a
dmi3y <dmitrii@matternet.us>
2023-10-15 22:25:52 -0700

    feat: add models (ignored)

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,8 +10,18 @@ const pages = defineCollection({
       wallpapers: z.array(image()),
     }),
 });
+const models = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      wallpapers: z.array(image()),
+    }),
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   pages,
+  models,
 };
